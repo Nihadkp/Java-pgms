d2b2bcaf390954bfd7aaf057dd55983e46ada26c
Nihadkp <78895436+Nihadkp@users.noreply.github.com>
2021-08-29 11:26:08 +0530

    Update lowprice.java

--- CO1/lowprice.java
+++ CO1/lowprice.java
@@ -1,19 +1,38 @@
-class complex
+class product
 {
-	int real,image;
-	void add(int a,int b,int c,int d)
+	int pcode;
+    	String pname;
+    	double price;
+    	product(int pcode,String pname,double price)	
 	{
-		real=a+c;
-		image=b+d;
-		System.out.println("complex number is "+real+" + "+image+"i");
+    	this.pcode=pcode;
+    	this.pname=pname;
+    	this.price=price;    
 	}
+	void low(product p1,product p2,product p3) 
+	{
+    		if ((p1.price<p2.price) && (p1.price<p3.price))
+        	{
+           		System.out.println("lowest price is : "+p1.price);
+        	}
+        	else if ((p2.price<p1.price) && (p2.price<p3.price))
+        	{
+         		System.out.println("lowest price is : "+p2.price);
+
+	        }
+        	else
+        	{
+          		System.out.println("lowest price is : "+p3.price);
+	        }
+        }
 }
-public class complexadd
+public class lowprice
 {
-	public static void main(String[] Args)
+    	public static void main(String[] args) 
 	{
-		complex c=new complex();
-		c.add(3,5,6,7);
-	}
+        	product p1=new product(101,"laptop",50000);
+        	product p2=new product(203,"pendrive",500);
+        	product p3=new product(504,"keyboard",670);
+        	p3.low(p1,p2,p3);
+        }
 }
-	
